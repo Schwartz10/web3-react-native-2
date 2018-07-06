df014e7ba82cd1aa896d1cad8a2dce66cd2a94c8
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2018-07-06 13:54:47 -0400

    changes to truffle boxes initial state, adds redux logging

--- src/store.js
+++ src/store.js
@@ -1,7 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createLogger } from 'redux-logger';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(
+  thunk,
+  createLogger({ stateTransformer: state => state }))));
 
 export default store;
