b19234ceea103533ef90a31d0d5003a03a47030c
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2018-07-21 14:30:21 -0400

    first commit

--- android/app/src/main/java/com/web3reactnative/MainApplication.java
+++ android/app/src/main/java/com/web3reactnative/MainApplication.java
@@ -3,6 +3,9 @@ package com.web3reactnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.tradle.react.UdpSocketsModule;
+import com.peel.react.TcpSocketsModule;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.oblador.keychain.KeychainPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new UdpSocketsModule(),
+            new TcpSocketsModule(),
             new RandomBytesPackage(),
             new KeychainPackage()
       );
